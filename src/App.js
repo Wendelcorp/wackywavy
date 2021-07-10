b7b3d8fa0455c6409823ddc538d9ba2243c9c47f
Bryce Wendelaar <bryce@brycewendelaar.com>
2021-07-10 13:16:13 -0400

    working demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
 import './App.css';
+import Wacky from './Wacky'
+import Wackybuilder from './Wackybuilder'
 
-function App() {
+export default class App extends React.Component {
+
+
+  render() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={"container"}>
+    <Router>
+    <div>
+      <Switch>
+        <Route path="/wacky">
+          <Wacky />
+        </Route>
+        <Route path="/">
+          <Wackybuilder />
+        </Route>
+      </Switch>
     </div>
-  );
+  </Router>
+    </div>
+  )}
 }
-
-export default App;
---
+++ src/Wackybuilder.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import Lottie from 'react-lottie';
+import { replaceColor } from 'lottie-colorify';
+import { colorify } from 'lottie-colorify';
+import { getColors } from 'lottie-colorify';
+import { TwitterPicker } from 'react-color';
+import { CopyBlock, dracula } from "react-code-blocks";
+import animationData from './data/floatingguy';
+import './App.css';
+
+
+export default class Wackybuilder extends React.Component {
+
+  state = {
+  background: 'EB144C',
+  };
+
+  handleChangeComplete = (color) => {
+    this.setState({ background: color.hex.replace('#', '') });
+  };
+
+  render() {
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: replaceColor([231, 95, 59], `${this.state.background}`, animationData),
+        rendererSettings: {
+          preserveAspectRatio: "xMidYMid slice"
+        }
+      }
+    const code = `<iframe width="300" height="400" src="http://localhost:3001/wacky?color=${this.state.background}" title="Wacky Wavy" frameborder="0"></iframe>`
+    console.log(getColors(animationData));
+  return (
+    <div className={"container"}>
+    <h3>You Can't Have a Sale Without a</h3>
+    <h1>Wacky Wavy</h1>
+      <Lottie
+	    options={defaultOptions}
+        height={400}
+        width={400}
+      />
+      <div className={"embed-code"}>
+        <CopyBlock
+          className={"embed-code"}
+          text={code}
+          language={"HTML"}
+          showLineNumbers={false}
+          startingLineNumber={1}
+          theme={dracula}
+          wrapLines
+        />
+      </div>
+      <TwitterPicker color={ this.state.background }
+        onChangeComplete={ this.handleChangeComplete }/>
+    </div>
+  )}
+}
